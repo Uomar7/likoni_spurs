7cdd7afacb326699844f90edb432bbff5abb7637
Uomar <uomarearlie7@gmail.com>
2020-04-24 14:11:24 +0300

    Created the home view

--- player/views.py
+++ player/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.views.generic import TemplateView, View
+from .models import Player
 
-# Create your views here.
+class HomeView(TemplateView):
+     template_name = 'player/index.html'
+
+     def get_context_data(self, *args, **kwargs):
+          context = super().get_context_data(*args, **kwargs)
+          context['players'] = Player.objects.all()
+          return context
