d6c2f6ea642e1018d8d9518fcc65177eb8fe30f4
Uomar <uomarearlie7@gmail.com>
2020-04-24 16:45:51 +0300

    Created the player detail view

--- player/views.py
+++ player/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render,get_object_or_404, get_list_or_404
-from django.views.generic import TemplateView, View, ListView
 from .models import Player
+from django.views.generic import (
+     ListView,
+     DetailView,
+)
+
 
 class PlayersList(ListView):
      model = Player
      template_name = 'players/index.html'
      context_object_name = 'players'
+
+class PlayerDetail(DetailView):
+     model = Player
+     template_name = 'players/player_detail.html'
+     context_object_name = 'player'
