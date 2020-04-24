f356562c4de67ec44e1970b2016240adc0065296
Uomar <uomarearlie7@gmail.com>
2020-04-24 17:01:24 +0300

    Created the url for the detail view

--- player/urls.py
+++ player/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
      PlayersList,
+     PlayerDetail,
 )
 
 urlpatterns = [
      path('players/',PlayersList.as_view(), name='home'),
+     path('player_detail/<int:pk>/', PlayerDetail.as_view(), name='detail'),
 ]
